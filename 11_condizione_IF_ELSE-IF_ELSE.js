57403f8dbb698ffb52940f6174c0a1adc985d5a0
Giovanni <desiderio.giovanni92@gmail.com>
2020-09-25 17:33:39 +0200

    condizioni varie

---
+++ 11_condizione_IF_ELSE-IF_ELSE.js
@@ -0,0 +1,33 @@
+//Istruzione condizionale IF
+
+if (3>2) {
+    console.log("E' vero");
+  } else{
+     console.log("Non E' vero");
+   
+  }
+
+
+   
+ var voto =prompt('Inserire il  tuo voto');
+   voto = parseInt(voto);
+ 
+    if(isNaN(voto)){
+      alert('Non hai inserito un valore numerico');
+    }
+ 
+    else if(voto>0 && voto <5){
+        alert('Votro troppo basso: bocciato!');
+    }
+ 
+    else if(voto>5 && voto<=8){
+        alert('Un voto non male!');
+    }
+    else if(voto>8 && voto <=10){
+      alert('Eccelente');
+    }
+    
+    else {
+        alert('Valore non valido');
+    }
+    
---
+++ 12_condizione_SWITCH.js
@@ -0,0 +1,30 @@
+var paese ='';
+var lingua = '';
+
+switch(paese){
+  case 'ITALIA': 
+      lingua = 'it';
+       console.log(lingua);
+    
+    break;
+    case 'CUBA' :
+   
+    case 'CILE' :
+    lingua = 'es';
+    break;
+    default :
+     lingua ='en';
+}
+
+switch(5<3){
+  case true: 
+      
+       console.log('La condizione switch è true');
+   break;
+ case false:
+    console.log('La condizione switch è false');
+   break;
+   
+}
+
+//console.log('La lingua di '+paese +' è '+ lingua);
